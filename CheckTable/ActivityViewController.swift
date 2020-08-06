390568585503a607df92d6830d92a81e32787759
Aki-373 <kusamoch@gmail.com>
2020-08-06 17:42:34 +0900

    ActivityViewController追加

---
+++ CheckTable/ActivityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ActivityViewController.swift
+//  CheckTable
+//
+//  Created by 甲田明寛 on 2020/08/06.
+//  Copyright © 2020 Akihiro Koda. All rights reserved.
+//
+
+import UIKit
+
+class ActivityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
