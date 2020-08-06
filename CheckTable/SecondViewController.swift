ce37bc3444ab36c24932c2710c010d3bc8aa8334
Aki-373 <kusamoch@gmail.com>
2020-08-06 17:18:36 +0900

    Initial Commit

---
+++ CheckTable/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CheckTable
+//
+//  Created by 甲田明寛 on 2020/08/06.
+//  Copyright © 2020 Akihiro Koda. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
