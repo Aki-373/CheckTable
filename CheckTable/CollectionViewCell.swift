bd6d3ef4728b37655a600e80a853ccf6cd3192a4
Aki-373 <kusamoch@gmail.com>
2020-08-09 16:22:44 +0900

    table10%くらい

---
+++ CheckTable/CollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CollectionViewCell.swift
+//  CheckTable
+//
+//  Created by 甲田明寛 on 2020/08/09.
+//  Copyright © 2020 Akihiro Koda. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var number: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        // cellの枠の太さ
+        self.layer.borderWidth = 1.0
+        // cellの枠の色
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+}
--- CheckTable/FirstViewController.swift
+++ CheckTable/FirstViewController.swift
@@ -10,29 +10,39 @@ import UIKit
 
 class FirstViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
     
+    var numbers = [String]()
+    
     @IBOutlet var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for i in 1 ..< 200 {
+            numbers.append(String(i))
+        }
         let layout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        layout.scrollDirection = UICollectionView.ScrollDirection.vertical
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 5
         collectionView.collectionViewLayout = layout
         
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 18 // 表示するセルの数
+        return numbers.count // 表示するセルの数
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) // 表示するセルを登録(先程命名した"Cell")
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)as! CollectionViewCell// 表示するセルを登録(先程命名した"Cell")
+        
+        cell.number.text = numbers[indexPath.item]
+        
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let horizontalSpace : CGFloat = 20
-        let cellSize : CGFloat = self.view.bounds.width / 3 - horizontalSpace
+        let cellSize = 35
         return CGSize(width: cellSize, height: cellSize)
     }
 //
