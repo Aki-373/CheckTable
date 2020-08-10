92b3d1201ef00ee1e9fa489c701cd080b953291a
2000lilu0317 <you@example.com>
2020-08-10 09:56:37 +0900

    めちゃめちゃエラーが生じる

---
+++ CheckTable/AddViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AddViewController.swift
+//  CheckTable
+//
+//  Created by 李　璐 on 2020/08/09.
+//  Copyright © 2020 Akihiro Koda. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class AddViewController: UIViewController {
+
+    @IBOutlet weak var contentTextView: UITextView!
+    var me: AppUser!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func postContent(_ sender: Any) {
+        let content = contentTextView.text!
+        let saveDocument = Firestore.firestore().collection("posts").document()
+        saveDocument.setData([
+            "content": content,
+            "postID": saveDocument.documentID,
+            "senderID": user.uid,
+            "createdAt": FieldValue.serverTimestamp(),
+            "updatedAt": FieldValue.serverTimestamp()
+        ]) { error in
+            if error == nil {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    func setupTextView() {
+        let toolBar = UIToolbar() // キーボードの上に置くツールバーの生成
+        let flexibleSpaceBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) // 今回は、右端にDoneボタンを置きたいので、左に空白を入れる
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissKeyboard)) // Doneボタン
+        toolBar.items = [flexibleSpaceBarButton, doneButton] // ツールバーにボタンを配置
+        toolBar.sizeToFit()
+        contentTextView.inputAccessoryView = toolBar // テキストビューにツールバーをセット
+    }
+    func setupTextView() {
+        let toolBar = UIToolbar() // キーボードの上に置くツールバーの生成
+        let flexibleSpaceBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) // 今回は、右端にDoneボタンを置きたいので、左に空白を入れる
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissKeyboard)) // Doneボタン
+        toolBar.items = [flexibleSpaceBarButton, doneButton] // ツールバーにボタンを配置
+        toolBar.sizeToFit()
+        contentTextView.inputAccessoryView = toolBar // テキストビューにツールバーをセット
+    }
+    // キーボードを閉じる処理。
+    @objc func dismissKeyboard() {
+        contentTextView.resignFirstResponder()
+    }
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
--- CheckTable/SecondViewController.swift
+++ CheckTable/SecondViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class SecondViewController: UIViewController {
 
+    var me: AppUser!
+    var database: Firestore! // 宣言
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        database = Firestore.firestore() // 初期値代入
     }
 
-
+    // 投稿追加画面に遷移するボタンを押したときの動作を記述。
+    
+    @IBAction func toAddViewController(_ sender: Any) {
+        performSegue(withIdentifier: "Add", sender: me)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! AddViewController // segue.destinationで遷移先のViewControllerが取得可能。
+        destination.me = sender as! AppUser
+    }
 }
 
