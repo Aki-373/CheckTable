28931d2720de03572cc539c6fa9b766ab9191852
2000lilu0317 <you@example.com>
2020-08-11 16:19:41 +0900

    ユーザー登録はできたけど画面遷移がうまくいかない😭

--- CheckTable/AccountViewController.swift
+++ CheckTable/AccountViewController.swift
@@ -8,17 +8,16 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class AccountViewController: UIViewController {
-
+    var auth: Auth!
     @IBOutlet weak var mailadderss: UILabel!
     
     @IBOutlet weak var password: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
-    
-        var auth: Auth! // 追加
 
         override func viewDidLoad() {
             super.viewDidLoad()
@@ -28,17 +27,45 @@ class AccountViewController: UIViewController {
         }
 
     // 登録ボタンを押したときに呼ぶメソッド。
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if auth.currentUser != nil {
+            auth.currentUser?.reload(completion: { error in
+                if error == nil {
+                    if self.auth.currentUser?.isEmailVerified == true {
+                        self.performSegue(withIdentifier: "Timeline", sender: self.auth.currentUser!)
+                    } else if self.auth.currentUser?.isEmailVerified == false {
+                        let alert = UIAlertController(title: "確認用メールを送信しているので確認をお願いします。", message: "まだメール認証が完了していません。", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            })
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextViewController = segue.destination as! SecondViewController
+        let user = sender as! User
+        nextViewController.me = AppUser(data: ["userID": user.uid])
+    }
+
         
     @IBAction func registerAccount(_ sender: Any) {
         let email = emailTextField.text!
             let password = passwordTextField.text!
             auth.createUser(withEmail: email, password: password) { (result, error) in
                 if error == nil, let result = result {
-                    // errorが nil であり、resultがnilではない == user情報がきちんと取得されている。
-                    self.performSegue(withIdentifier: "Timeline", sender: result.user)// 遷移先の画面でuser情報を渡している。
+                    result.user.sendEmailVerification(completion: { (error) in
+                        if error == nil {
+                            let alert = UIAlertController(title: "仮登録を行いました。", message: "入力したメールアドレス宛に確認メールを送信しました。", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        }
+                    })
                 }
             }
-    }
+        }
 }
 
     // デリゲートメソッドは可読性のためextensionで分けて記述します。
