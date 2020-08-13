05945621eee9bc230c0cdd6ac12abecf90a03df0
2000lilu0317 <you@example.com>
2020-08-13 19:55:54 +0900

    画像のアップロードまで成功した

--- CheckTable/AddViewController.swift
+++ CheckTable/AddViewController.swift
@@ -11,6 +11,8 @@ import Firebase
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
     @IBOutlet weak var contentTextView: UITextView!
     
     var me: AppUser!
@@ -27,7 +29,8 @@ class AddViewController: UIViewController {
     let Array = [Int](1...90)
     override func viewDidLoad() {
            super.viewDidLoad()
-           
+           imagePicker.delegate = self
+        contentTextView.layer.borderWidth = 0.5
            // ピッカー設定
            pickerView.delegate = self
            pickerView.dataSource = self
@@ -50,7 +53,39 @@ class AddViewController: UIViewController {
        @objc func done() {
            book_kind.endEditing(true)
            book_kind.text = "\(list[pickerView.selectedRow(inComponent: 0)])"
+        number.endEditing(true)
+        number.text = "\(Array[pickerView.selectedRow(inComponent: 1)])"
     }
+    @IBAction func presentImagePicker(_ sender: Any) {
+        imagePicker.allowsEditing = true //画像の切り抜きが出来るようになります。
+        imagePicker.sourceType = .photoLibrary //画像ライブラリを呼び出します
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func startUpload(_ sender: Any) {
+        upload()
+    }
+    fileprivate func upload() {
+    let date = NSDate()
+    let currentTimeStampInSecond = UInt64(floor(date.timeIntervalSince1970 * 1000))
+        let storageRef = Storage.storage().reference().child("images").child("\(currentTimeStampInSecond).jpg")
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpg"
+            if let uploadData = self.imageView.image?.jpegData(compressionQuality: 0.9) {
+                storageRef.putData(uploadData, metadata: metaData) { (metadata , error) in
+                    if error != nil {
+                        print("error: \(error?.localizedDescription)")
+                    }
+                    storageRef.downloadURL(completion: { (url, error) in
+                        if error != nil {
+                            print("error: \(error?.localizedDescription)")
+                        }
+                        print("url: \(url?.absoluteString)")
+                    })
+                }
+            }
+        }
     @IBAction func postContent(_ sender: Any) {
         let content = contentTextView.text!
         let book = book_kind.text!
@@ -134,3 +169,18 @@ extension AddViewController : UIPickerViewDelegate, UIPickerViewDataSource {
      
 }
 
+extension AddViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+ 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
