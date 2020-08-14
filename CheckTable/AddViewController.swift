61417113bc2e9cd2b2edd6dc88c782a86d631e30
ymsk <yamasaki-shun219@g.ecc.u-tokyo.ac.jp>
2020-08-15 00:52:15 +0900

    nikoniko

--- CheckTable/AddViewController.swift
+++ CheckTable/AddViewController.swift
@@ -7,24 +7,51 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseCore
 
 class AddViewController: UIViewController {
+    
+    @IBOutlet var contentTextView: UITextView! // 追加
+
+    var me: AppUser!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func postContent() {
+    }
+    
+    
+    func setupTextView() {
+        let toolBar = UIToolbar() // キーボードの上に置くツールバーの生成
+        let flexibleSpaceBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) // 今回は、右端にDoneボタンを置きたいので、左に空白を入れる
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissKeyboard)) // Doneボタン
+        toolBar.items = [flexibleSpaceBarButton, doneButton] // ツールバーにボタンを配置
+        toolBar.sizeToFit()
+        contentTextView.inputAccessoryView = toolBar // テキストビューにツールバーをセット
+    }
 
-        // Do any additional setup after loading the view.
+    // キーボードを閉じる処理。
+    @objc func dismissKeyboard() {
+        contentTextView.resignFirstResponder()
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBAction func postContent(_ sender: Any) {
+           let content = contentTextView.text!
+           let saveDocument = Firestore.firestore().collection("posts").document()
+           saveDocument.setData([
+               "content": content,
+               "postID": saveDocument.documentID,
+               //"senderID": user.uid
+               "createdAt": FieldValue.serverTimestamp(),
+               "updatedAt": FieldValue.serverTimestamp()
+           ]) { error in
+               if error == nil {
+                   self.dismiss(animated: true, completion: nil)
+               }
+           }
+       }
 }
--- CheckTable/TimelineViewController.swift
+++ CheckTable/TimelineViewController.swift
@@ -6,14 +6,61 @@
 //  Copyright © 2020 Akihiro Koda. All rights reserved.
 //
 
+
 import UIKit
-import Firebase // 追加
+import Firebase
+import FirebaseCore
+// デリゲート・データソースを記述。
+class TimelineViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var me: AppUser!
+    var database: Firestore! // 宣言
+    
+    @IBOutlet var tableView: UITableView! // 追加
 
-class TimelineViewController: UIViewController {
-
-    var me: AppUser! // 追加
- 
     override func viewDidLoad() {
         super.viewDidLoad()
+        database = Firestore.firestore()
+        tableView.delegate = self  // 追加
+        tableView.dataSource = self // 追加
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        cell.textLabel?.text = postArray[indexPath.row].content
+        return cell
+    }
+    
+
+//////////
+
+    // 投稿追加画面に遷移するボタンを押したときの動作を記述。
+    @IBAction func toAddViewController() {
+        performSegue(withIdentifier: "Add", sender: me)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! AddViewController // segue.destinationで遷移先のViewControllerが取得可能。
+        destination.me = sender as! AppUser
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        database.collection("posts").getDocuments { (snapshot, error) in
+            if error == nil, let snapshot = snapshot {
+                self.postArray = []
+                for document in snapshot.documents {
+                    let data = document.data()
+                    let post = Post(data: data)
+                    self.postArray.append(post)
+                }
+                self.tableView.reloadData() // 先ほど書いたprint文をこちらに変更
+            }
+        }
     }
 }
+
