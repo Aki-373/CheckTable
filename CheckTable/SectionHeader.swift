ae067865978bbfcc44c6219b15ce9570972fb0a5
Aki-373 <kusamoch@gmail.com>
2020-08-15 12:12:15 +0900

    一時避難

---
+++ CheckTable/SectionHeader.swift
@@ -0,0 +1,14 @@
+//
+//  SectionHeader.swift
+//  CheckTable
+//
+//  Created by 甲田明寛 on 2020/08/14.
+//  Copyright © 2020 Akihiro Koda. All rights reserved.
+//
+
+import UIKit
+
+class SectionHeader: UICollectionReusableView {
+    @IBOutlet weak var sectionLabel: UILabel!
+    
+}
