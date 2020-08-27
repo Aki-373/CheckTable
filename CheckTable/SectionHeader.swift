35986fa5c8ec7f629d735e6e2d92d2161091823d
ymsk <yamasaki-shun219@g.ecc.u-tokyo.ac.jp>
2020-08-27 15:31:12 +0900

    追加しました

--- CheckTable/SectionHeader.swift
+++ CheckTable/SectionHeader.swift
@@ -4,7 +4,19 @@
 //
 //  Created by 甲田明寛 on 2020/08/13.
 //  Copyright © 2020 Akihiro Koda. All rights reserved.
-//
+// 追加しました
+
+
+
+
+
+
+
+
+
+
+
+
 import UIKit
 
 class SectionHeader: UICollectionReusableView {
